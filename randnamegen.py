d707270d8592fe782023f52ebaec999302aa65e0
Alex <harrison.alex117@gmail.com>
2019-03-24 20:51:11 -0700

    wrapped code in main function

--- randnamegen.py
+++ randnamegen.py
@@ -3,18 +3,21 @@ import sys, random
 print("Welcome to the Psych 'Sidekick Name Picker.'\n")
 print("A name just like Sean would pick for Gus:\n\n")
 
-first = ('Baby Oil', 'Bad News', 'Big Burps')
-last = ('Appleyard', 'Bigmeat', 'Bloominshine')
+def main():
+    first = ('Baby Oil', 'Bad News', 'Big Burps')
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine')
 
-while True:
-    firstName = random.choice(first)
-    lastName = random.choice(last)
+    while True:
+        firstName = random.choice(first)
+        lastName = random.choice(last)
 
-    print("\n\n") #new line for readability
-    print("{} {}".format(firstName, lastName), file=sys.stderr)
-    print("\n\n")
+        print("\n\n") #new line for readability
+        print("{} {}".format(firstName, lastName), file=sys.stderr)
+        print("\n\n")
 
-    try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
-    if try_again.lower() == "n":
-        break #to quit the program otherwise it runs until user inputs "n"
-input("\nPress Enter to exit.")
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+        if try_again.lower() == "n":
+            break #to quit the program otherwise it runs until user inputs "n"
+    input("\nPress Enter to exit.")
+if __name__ == "__main__":
+    main()
