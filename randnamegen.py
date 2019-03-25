29efd7e18f2a2f07ca57f213a0e1dfd97e83d78f
Alex <harrison.alex117@gmail.com>
2019-03-24 20:31:20 -0700

    added some comments

--- randnamegen.py
+++ randnamegen.py
@@ -10,11 +10,11 @@ while True:
     firstName = random.choice(first)
     lastName = random.choice(last)
 
-    print("\n\n")
+    print("\n\n") #new line for readability
     print("{} {}".format(firstName, lastName), file=sys.stderr)
     print("\n\n")
 
     try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
     if try_again.lower() == "n":
-        break
+        break #to quit the program otherwise it runs until user inputs "n"
 input("\nPress Enter to exit.")
