d721bf2944278c2cc240091794a438472a34fee1
Alex <harrison.alex117@gmail.com>
2019-03-24 22:47:46 -0700

    new file comparing time function vs cProfile

---
+++ palin_dromes/palingrams_timed.py
@@ -0,0 +1,31 @@
+"""Find all word pair palingrams in a dictionary file."""
+import time
+start_time = time.time()
+import load_diction
+
+word_list = load_diction.load('2of4brif.txt')
+
+#find word pair palingrams
+def find_palingrams():
+    """Find dictionary palingrams"""
+    pali_list = []
+    for word in word_list:
+        end = len(word)
+        rev_word = word[::-1]#word.join(reversed(word)) produces different result, not sure why
+        if end > 1:
+            for i in range(end):
+                if word[i:] == rev_word[:end-1]and rev_word[end-1:] in word_list:
+                    pali_list.append((word, rev_word[end-1:]))
+                if word[:i] == rev_word[end-1:]and rev_word[:end-i] in word_list:
+                    pali_list.append((rev_word[:end-1], word))
+    return pali_list
+palingrams = find_palingrams()
+#sort palingrams alphabetically
+palingrams_sorted = sorted(palingrams)
+
+#display list of palingrams
+print("\nNumber of palingrams = {}\n".format(len(palingrams_sorted)))
+for first, second in palingrams_sorted:
+    print("{} {}".format(first, second))
+end_time = time.time()
+print("Runtime for this program was {} seconds.".format(end_time - start_time))
